bcee15e4e68af72c2d8d4a92bf4597971b55569d
John Coote <john_coote@rocketmail.com>
2018-01-04 23:48:07 +1100

    ok for live

--- js/main.js
+++ js/main.js
@@ -4,11 +4,16 @@ let w = 0 // window width
 let h = 0 // window height
 let n = 0 // number of boxes per row / column
 let x = 0 // for sine calculations
+let smallBoxSize = 20 // small box size
 let pie = Math.PI
 let rateFactor = 0.1 // anglular distance - integer multiples of pi will give static colors
 let speedConstant = 100 // how often to call the call the setInterval function
 let pulseTimerDone
 let rainbowTimerDone
+let heartbeatTimerDone
+let diagonalTimerDone
+let mesmeriseTimerDone
+let mesmer2TimerDone
 
 const drawBigSquare = function(s) {
   let bigSquareDiv = `<div class="bigSquare"></div>`
@@ -42,7 +47,7 @@ const drawBoxes = function() {
   if (h < w) { bigSquareSize = h }
   drawBigSquare(bigSquareSize)
 
-  drawSmallBoxes(20, bigSquareSize)
+  drawSmallBoxes(smallBoxSize, bigSquareSize)
 };
 
 
@@ -67,20 +72,84 @@ const pulse = function () {
 
 const rainbow = function () {
   for (var i = 1; i <= n; i++) {
-    x = x + rateFactor + (pie / n );
+    x = x + rateFactor + (pie / n);
     let r = Math.round((127 * Math.sin(x) + 128))
     let g = Math.round((127 * Math.sin(x + (pie/3)) + 128))
     let b = Math.round((127 * Math.sin(x - (pie/3)) + 128))
-    // console.log(r, g, b);
-
-    $(`.smallSquare.col-${i}`).css('background-color', `rgb(${r},${g},${b})`)
-    $('.smallSquare').css('border', 'none')
+    $(`.smallSquare.row-${i}`).css('background-color', `rgb(${r},${g},${b})`)
   }
-
-
-
-
+  $('.smallSquare').css('border', 'none')
 }
+
+
+const heartbeat = function () {
+  for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= n; j++) {
+      x = x + rateFactor / 1000
+      p = ((n/2-i) * (n/2-i))
+      q = ((n/2-j) * (n/2-j))
+      let r = Math.round((128 * Math.sin( x * (p + q)) + 127))
+      let g = Math.round((128 * Math.cos( x * (p + q)) + 127))
+      let b = Math.round((127 * Math.sin(x) + p + q))
+      // x = ((i*i) + (j*j))
+
+
+      $(`.smallSquare.col-${i}.row-${j}`).css('background-color', `rgb(255,${b},${b})`)
+    }
+  }
+  $('.smallSquare').css('border', 'none')
+}
+
+
+const mesmerise = function () {
+  for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= n; j++) {
+      x = x + rateFactor / 10000
+      p = ((i) * (i))
+      q = ((j) * (j))
+      let r = Math.round((127 * Math.sin( x * (p + q) / n) + 128))
+
+      $(`.smallSquare.col-${i}.row-${j}`).css('background-color', `rgb(0,${r},0)`)
+    }
+  }
+  $('.smallSquare').css('border', 'none')
+}
+
+
+const mesmer2 = function () {
+  for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= n; j++) {
+      x = x + rateFactor / 10000
+      p = ((n/2-i) * (n/2-i))
+      q = ((n/2-j) * (n/2-j))
+      let r = Math.round((128 * Math.sin( x * (p + q)) + 127))
+      let g = Math.round((128 * Math.cos( x * (p + q)) + 127))
+
+      $(`.smallSquare.col-${i}.row-${j}`).css('background-color', `rgb(${r},${g},0)`)
+    }
+  }
+  $('.smallSquare').css('border', 'none')
+}
+
+
+
+
+
+const diagonal = function () {
+  for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= n; j++) {
+      x = x + (rateFactor) + (pie / n);
+      let r = Math.round((127 * Math.sin(x) + 128))
+      let g = Math.round((127 * Math.sin(x + (pie/2)) + 128))
+      let b = Math.round((127 * Math.sin(x - (pie/2)) + 128))
+      $(`.smallSquare.col-${i}.row-${j}`).css('background-color', `rgb(${r},${g},${b})`)
+    }
+  }
+  $('.smallSquare').css('border', 'none')
+}
+
+
+
 
 
 $(document).ready( function () {
@@ -115,55 +184,49 @@ $(document).ready( function () {
     rainbowTimerDone = setInterval(rainbow, speedConstant);
   })
 
+  $('#diagonal').on('click', function() {
+    getValues()
+    stopAndRedraw();
+    diagonalTimerDone = setInterval(diagonal, speedConstant);
+  })
+
+
+  $('#heartbeat').on('click', function() {
+    getValues()
+    stopAndRedraw();
+    heartbeatTimerDone = setInterval(heartbeat, speedConstant);
+  })
+
+  $('#mesmerise').on('click', function() {
+    getValues()
+    stopAndRedraw();
+    mesmeriseTimerDone = setInterval(mesmerise, speedConstant);
+  })
+
+  $('#mesmer2').on('click', function() {
+    getValues()
+    stopAndRedraw();
+    mesmer2TimerDone = setInterval(mesmer2, speedConstant);
+  })
+
+
   const stopAndRedraw = function () {
+    x = 0
     clearInterval(pulseTimerDone);
     clearInterval(rainbowTimerDone);
+    clearInterval(heartbeatTimerDone);
+    clearInterval(diagonalTimerDone);
+    clearInterval(mesmeriseTimerDone);
+    clearInterval(mesmer2TimerDone);
+
     $('.right').html('')
     drawBoxes(w, h)
   }
 
   const getValues = function () {
     speedConstant = +( $('#interval').val() );
-    console.log(speedConstant);
+    smallBoxSize = +( $('#squareSize').val() );
+    console.log(speedConstant, smallBoxSize);
   }
 
 })
-
-
-
-
-
-
-
-
-
-//
-
-
-// let resizeTimer;
-// $(window).on('resize', function(e) {
-//
-//   clearTimeout(resizeTimer);
-//   resizeTimer = setTimeout(function() {
-//     w = $('.right').width();
-//     h = $('.right').height();
-//     drawBoxes(w, h);
-//     console.log("not quyite qorking yet");
-//   }, 250);
-//
-// });
-
-
-
-//
-// $(window).on('resize', function () {
-//   var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
-//
-//   if (scrollBottom > 600) {
-//     return; // Don't do anything until we are within 600 pixels of the bottom of the page.
-//   }
-//
-//   var query = $('#query').val();
-//   throttledSearchFlickr(query);
-//   console.log("resize");
-// });
