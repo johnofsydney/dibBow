bb7ba71da46032685f61a7492fee5012f87a882e
John Coote <john_coote@rocketmail.com>
2018-01-04 19:48:07 +1100

    rainbow

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ console.log("connected");
 let w = 0 // window width
 let h = 0 // window height
 let n = 0 // number of boxes per row / column
+let x = 0 // for sine calculations
+let pie = Math.PI
+let rateFactor = 0.1 // anglular distance - integer multiples of pi will give static colors
+let speedConstant = 100 // how often to call the call the setInterval function
+let pulseTimerDone
+let rainbowTimerDone
 
 const drawBigSquare = function(s) {
   let bigSquareDiv = `<div class="bigSquare"></div>`
@@ -16,7 +22,7 @@ const drawBigSquare = function(s) {
 
 const drawSmallBoxes = function(s,c) {
   console.log(s,c);
-  n = ((c - (c%s)) / s);
+  n = Math.round(c / s * 0.8);
   let smallSquareDiv = ''
 
   for (let i = 1; i <= n; i++) {
@@ -32,7 +38,6 @@ const drawSmallBoxes = function(s,c) {
 
 const drawBoxes = function() {
 
-  console.log(`height: ${h}, width: ${w}`);
   let bigSquareSize = w;
   if (h < w) { bigSquareSize = h }
   drawBigSquare(bigSquareSize)
@@ -43,10 +48,38 @@ const drawBoxes = function() {
 
 const simpleGradient = function () {
   for (var i = 1; i <= n; i++) {
-    let x = Math.round((255 - (i * 255/ n)))
-    $(`.smallSquare.col-${i}`).css('background-color', `rgb(${x},${x},${x})`)
+    let r = Math.round((255 - (i * 255/ n)))
+    $(`.smallSquare.col-${i}`).css('background-color', `rgb(${r},${r},${r})`)
     $('.smallSquare').css('border', 'none')
   }
+}
+
+const pulse = function () {
+  x = x + rateFactor * 3;
+  let r = Math.round((127 * Math.sin(x) + 128))
+  let g = Math.round((127 * Math.sin(x + (pie/3)) + 128))
+  let b = Math.round((127 * Math.sin(x - (pie/3)) + 128))
+  // console.log(r, g, b);
+  $(`.smallSquare`).css('background-color', `rgb(${r},${g},${b})`)
+  $('.smallSquare').css('border', 'none')
+}
+
+
+const rainbow = function () {
+  for (var i = 1; i <= n; i++) {
+    x = x + rateFactor + (pie / n );
+    let r = Math.round((127 * Math.sin(x) + 128))
+    let g = Math.round((127 * Math.sin(x + (pie/3)) + 128))
+    let b = Math.round((127 * Math.sin(x - (pie/3)) + 128))
+    // console.log(r, g, b);
+
+    $(`.smallSquare.col-${i}`).css('background-color', `rgb(${r},${g},${b})`)
+    $('.smallSquare').css('border', 'none')
+  }
+
+
+
+
 }
 
 
@@ -56,27 +89,69 @@ $(document).ready( function () {
   h = $('.right').height();
   drawBoxes(w, h);
 
-  $('#simpleGradient').on('click', function(e) {
-    // e.preventDefault();
+
+  $('#simpleGradient').on('click', function() {
+    getValues()
+    stopAndRedraw();
     simpleGradient();
   })
 
+  $('#pulse').on('click', function() {
+    getValues()
+    stopAndRedraw();
+    pulseTimerDone = setInterval(pulse, speedConstant);
+  })
+
+  $('#stopPulse').on('click', function() {
+    clearInterval(pulseTimerDone);
+    clearInterval(rainbowTimerDone);
+    $('.right').html('')
+    drawBoxes(w, h)
+  })
+
+  $('#rainbow').on('click', function() {
+    getValues()
+    stopAndRedraw();
+    rainbowTimerDone = setInterval(rainbow, speedConstant);
+  })
+
+  const stopAndRedraw = function () {
+    clearInterval(pulseTimerDone);
+    clearInterval(rainbowTimerDone);
+    $('.right').html('')
+    drawBoxes(w, h)
+  }
+
+  const getValues = function () {
+    speedConstant = +( $('#interval').val() );
+    console.log(speedConstant);
+  }
 
 })
+
+
+
+
+
+
+
+
 
 //
 
 
-let resizeTimer;
-$(window).on('resize', function(e) {
-
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(function() {
-    drawBoxes();
-    console.log("not quyite qorking yet");
-  }, 1250);
-
-});
+// let resizeTimer;
+// $(window).on('resize', function(e) {
+//
+//   clearTimeout(resizeTimer);
+//   resizeTimer = setTimeout(function() {
+//     w = $('.right').width();
+//     h = $('.right').height();
+//     drawBoxes(w, h);
+//     console.log("not quyite qorking yet");
+//   }, 250);
+//
+// });
 
 
 
